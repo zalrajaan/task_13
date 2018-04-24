47c6f513873e5f2039ca4b6ddc99502731eaeae5
DarthHamza <hamzamakia@gmail.com>
2018-04-24 10:15:12 +0300

    FavoriteRestaurant model created

--- restaurants/models.py
+++ restaurants/models.py
@@ -24,5 +24,5 @@ class Item(models.Model):
 
 
 class FavoriteRestaurant(models.Model):
-    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='favourits')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favorited_by')
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
